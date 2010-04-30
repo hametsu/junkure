ad5e02d35b07f53aeb8c72d6c227cde95a1c502d
yuiseki <yuiseki@gmail.com>
2010-05-01 01:31:14 +0900

    update to release

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,12 +1,31 @@
 class IndexController < ApplicationController
-  def show
-    @user = User.find_by_login(params["login"])
-    render :file=> "public/404.html", :status=>'404 Not Found' and return unless @user
+  layout "index"
+
+  def admin
+    if check_admin(current_user.login)
+      @users = User.find(:all, :order => "updated_at DESC", :conditions => ["junk is NOT NULL", ])
+    else
+      render :file=> "public/404.html", :status=>'404 Not Found'
+    end
   end
 
-  def users
-    @users = User.find(:all, :order => "updated_at DESC")
+  def top
+    @users = User.find(:all, :order => "created_at DESC", :conditions => ["junk is NOT NULL", ])
+    @change = false
+    @change = true if params.key?(:change)
   end
 
+  def update
+    user = params[:user]
+    current_user.address = user[:address][0, 256]
+    current_user.tel = user[:tel][0, 256]
+    current_user.junk = user[:junk][0, 1024]
+    current_user.save!
+    redirect_to :action => :top
+  end
 
+  private
+  def check_admin(login_name)
+    return ['lie_', 'tetsuomi', 'kaichoo', 'yuiseki', 'itkz', 'itoyanagi', 'hagino3000'].include?(login_name)
+  end
 end
