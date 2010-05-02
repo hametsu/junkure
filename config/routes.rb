7ec98e51d8ead99ae32b26eab70b5e7026d1ea97
TAKANO Mitsuhiro <takano32@gmail.com>
2010-05-02 18:44:51 +0900

    add find_by_way route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-
-  map.root :controller => "index", :action => "top"
-  map.connect '/:action', :controller => "index", :action => :action
-
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-
+   map.root :controller => "index", :action => "top"
+   map.connect '/:action', :controller => "index", :action => :action
+   map.connect '/way/:name', :controller => "index", :action => 'find_by_way'
+   map.connect ':controller/:action/:id'
+   map.connect ':controller/:action/:id.:format'
 end
